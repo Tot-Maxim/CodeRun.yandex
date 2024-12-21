f6bf3efd1d638f373ff4289ef740a5f2773dc1de
TotMaxim <p.maxim.mail@yandex.ru>
2024-12-21 15:00:58 +0300

    add workflows

--- tskv_to_json.py
+++ tskv_to_json.py
@@ -1,27 +1,31 @@
 import json
 
-def tskv_to_json():
-    tskv_dict = {}
-    
-    print("Введите значения в формате key=value (или нажмите Enter, чтобы выйти):")
-    
-    while True:
-        entry = input()  # Считываем ввод
-        if entry.strip() == "":
-            break  # Выход из цикла при пустой строке
-        
-        # Разделяем ключ и значение
-        try:
-            key, value = entry.split('=', 1)  # Разделяем только по первому =, чтобы поддерживать значения с =
-            tskv_dict[key.strip()] = value.strip()  # Добавляем в словарь
-        except ValueError:
-            print("Неверный формат. Пожалуйста, используйте формат key=value.")
-    
-    # Преобразование словаря в JSON
-    json_output = json.dumps(tskv_dict, ensure_ascii=False, indent=4)
-    
-    print("Результирующий JSON:")
-    print(json_output)
 
-# Запускаем функцию
-tskv_to_json()
+def tskv_to_json():
+
+    tskv_dict = {}
+
+    print("Введите значения в формате key=value (или нажмите Enter, чтобы выйти):")
+
+    while True:
+
+        entry = input()  # Считываем ввод
+
+        if entry.strip() == "":
+
+            break  # Выход из цикла при пустой строке
+
+        try:
+
+            key, value = entry.split('=', 1)  # Разделяем только по первому =, чтобы поддерживать значения с =
+
+            tskv_dict[key.strip()] = value.strip()  # Добавляем в словарь
+
+        except ValueError:
+
+            print("Неверный формат. Пожалуйста, используйте формат key=value.")
+
+
+    json_output = json.dumps(tskv_dict, ensure_ascii=False, indent=4)
+
+    print(json_output)
