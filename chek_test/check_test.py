515b969bc88a4e78b19331048536b53d9862b049
TotMaxim <p.maxim.mail@yandex.ru>
2024-12-21 15:12:09 +0300

    add workflows

--- chek_test/check_test.py
+++ chek_test/check_test.py
@@ -7,7 +7,7 @@ import os
 
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-from tskv_to_json import tskv_to_json
+from tasks.tskv_to_json import tskv_to_json
 
 
 class TestTskvToJson(unittest.TestCase):
