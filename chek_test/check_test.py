9ec9543c366bee97e5f5a0e4c12a24b67c26c126
TotMaxim <p.maxim.mail@yandex.ru>
2024-12-21 14:51:15 +0300

    add test

--- chek_test/check_test.py
+++ chek_test/check_test.py
@@ -1,47 +1,36 @@
+import json
 import unittest
 from unittest.mock import patch
 from io import StringIO
-import json
 import sys
 import os
 
-# Добавляем путь к родительскому каталогу, чтобы импортировать tskv_to_json.py
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-# Импорт функции для тестирования
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from tskv_to_json import tskv_to_json
 
 
 class TestTskvToJson(unittest.TestCase):
 
     @patch('builtins.input', side_effect=['key1=value1', 'key2=value2', ''])
-
     @patch('sys.stdout', new_callable=StringIO)
 
-    def test_tskv_to_json(self, mock_stdout, mock_input):
-
+    def test_valid_input(self, mock_stdout, mock_input):
         tskv_to_json()
-
-        # Получаем выведенный результат
-
         output = mock_stdout.getvalue()
-
-
-        # Ожидаемый результирующий JSON
-
         expected_json = {
-
             "key1": "value1",
-
             "key2": "value2"
-
         }
+        self.assertIn(json.dumps(expected_json, ensure_ascii=False, indent=4), output)
+    @patch('builtins.input', side_effect=[''])
+    @patch('sys.stdout', new_callable=StringIO)
 
-
-        # Проверяем, что вывод содержит ожидаемый JSON
-
-        self.assertIn("Результирующий JSON:", output)
-
+    
+    def test_empty_input(self, mock_stdout, mock_input):
+        tskv_to_json()
+        output = mock_stdout.getvalue()
+        expected_json = {}
         self.assertIn(json.dumps(expected_json, ensure_ascii=False, indent=4), output)
 
 
